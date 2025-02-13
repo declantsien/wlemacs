2a9e261420bd5bfe757be92d5c75330d87679ef9
Declan Tsien <declantsien@riseup.net>
2025-02-13 15:15:15 +0800

    rm emacs.rs

--- src/emacs.rs
+++
@@ -1,5 +0,0 @@
-// Include the main c_exports file that holds the main rust_init_syms.
-// This function calls the other crates init_syms functions which contain
-// the generated bindings.
-#[cfg(not(test))]
-include!(concat!(env!("OUT_DIR"), "/c_exports.rs"));
